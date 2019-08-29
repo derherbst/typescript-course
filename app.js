e27d181213cdfcb0a98a7ff12fb2a8fdac18ae68
Georgii Gerbst <Georgii_Gerbst@epam.com>
2019-08-29 21:02:38 +0300

    functions arguments

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
 "use strict";
 console.log(123);
+var myName = 'Georg';
+// myName = 123;
+var myAge;
+myAge = 123;
+// myAge = 'tttt';
+var hobbies = ['reading', 'sports'];
+hobbies = [123];
+// hobbies = 123
+// tupels 
+var address = ['Kievskaya', 22];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
+console.log(Color);
+// any
+var car = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    console.log('Hello!');
+}
+// function arguments
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(2, 'G'));
+console.log(multiply(2, 10));
--- app.ts
+++ app.ts
@@ -1,1 +1,50 @@
 console.log(123);
+let myName = 'Georg';
+// myName = 123;
+let myAge: number;
+myAge = 123;
+// myAge = 'tttt';
+
+let hobbies: any[] = ['reading', 'sports'];
+hobbies = [123]
+// hobbies = 123
+
+// tupels 
+let address: [string, number] = ['Kievskaya', 22];
+
+// enum
+enum Color {
+    Red,
+    Green = 100,
+    Blue,
+}
+
+let myColor: Color = Color.Blue;
+console.log(myColor);
+console.log(Color);
+
+// any
+let car: any = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
+
+// functions
+
+function returnMyName(): string {
+    return myName;
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+    console.log('Hello!');
+}
+
+// function arguments
+function multiply(value1: number, value2: number): number {
+    return value1*value2;
+}
+
+// console.log(multiply(2, 'G'));
+console.log(multiply(2, 10 ));
